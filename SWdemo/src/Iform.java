fee988acbaaf633806fbbf7d328bd42075427d34
Ranahossam156 <ranahossam156@gmail.com>
2022-12-05 07:52:49 +0200

    .

---
+++ SWdemo/src/Iform.java
@@ -0,0 +1,11 @@
+public interface Iform {
+    void setPhoneNum();
+    int getPhoneNum();
+   int GetPhoneNum() ;
+    void SetName(String Name) ;
+    String GetName() ;
+
+
+
+
+}
---
+++ SWdemo/src/IserviceProvider.java
@@ -0,0 +1,2 @@
+public class IserviceProvider {
+}
