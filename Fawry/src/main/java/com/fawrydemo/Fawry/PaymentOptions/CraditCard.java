075fef1128c692fd7589c512fbcff8a51c7f864e
Ranahossam156 <ranahossam156@gmail.com>
2023-01-01 20:47:34 +0200

    small update 2

--- Fawry/src/main/java/com/fawrydemo/Fawry/PaymentOptions/CraditCard.java
+++ Fawry/src/main/java/com/fawrydemo/Fawry/PaymentOptions/CraditCard.java
@@ -4,13 +4,15 @@ public class CraditCard implements IpaymentOptions {
 	 private double balance = 1000;
 	@Override
 	public Boolean pay(double amount) {
+		boolean bool=false;
 		if(balance>=amount)
 		{
 
 			balance-=amount;
-			return true;
+			bool=true;
+			return bool;
 		}
-		return false;
+		return bool;
 	}
 
 	public double GetBalance() {
